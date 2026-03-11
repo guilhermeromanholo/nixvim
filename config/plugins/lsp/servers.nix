5d0068036bf3f46189a880a206de66af5b2c3eba
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-03-11 16:18:21 -0300

    fix

--- config/plugins/lsp/servers.nix
+++ config/plugins/lsp/servers.nix
@@ -1,19 +1,16 @@
 {
   lsp.inlayHints.enable = true;
+  lsp.servers.html.config.filetypes = ["html" "php"];
 
   plugins.lsp = {
     enable = true;
 
     servers = {
+      html.enable = true;
       nixd.enable = true;
       cssls.enable = true;
       pyright.enable = true;
       phpactor.enable = true;
-
-      html = {
-	enable = true;
-	config.filetypes = ["html" "php"];
-      };
     };
   };
 }
